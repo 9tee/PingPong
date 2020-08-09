197e24db519f3f90d6a3c7965c66171c783f4ed4
T-DucDung <cbcbvn99@mail.com>
2020-08-10 00:43:45 +0700

    oop js commit

--- script.js
+++ script.js
@@ -1,29 +1,91 @@
-class Line{
-    constructor(x,y,len){
-    this.x = x;
-    this.y = y;
-    this.lenght = len;
+class Line {
+    constructor(x, y, len) {
+        this.x = x;
+        this.y = y;
+        this.lenght = len;
     }
-    DrawLine() {
+    draw() {
         var c = document.getElementById("canvas");
         var ctx = c.getContext("2d");
         ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x, this.y+this.lenght);
+        ctx.lineTo(this.x, this.y + this.lenght);
         ctx.stroke();
     }
-    MoveLine(x,y){
+    moveLine(x, y) {
         this.x = x;
         this.y = y;
-        this.DrawLine()
+    }
+}
+function Clear() {
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.height, c.width);
+}
+function Draw(list) {
+    list.forEach(item => {
+        item.draw();
+    });
+}
+
+class Vectoc {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    setVector(x, y) {
+        this.x = x;
+        this.y = y;
     }
 }
 
-window.addEventListener('load',main)
+class Ball {
+    constructor(x, y, vector) {
+        this.x = x;
+        this.y = y;
+        this.vector = vector;
+    }
+    move() {
+        this.x += this.vector.x
+        this.y += this.vector.y
+    }
+    draw() {
+        var c = document.getElementById("canvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 3, 0, 2 * Math.PI);
+        ctx.fillStyle = "blue";
+        ctx.fill();
+    }
+}
+
+window.addEventListener('load', main);
+window.addEventListener('keypress', reDraw);
+
+var list = []
 
 function main() {
-    line1 = new Line(10,40,40)
-    line1.DrawLine()
-    line1.MoveLine(20,40)
+    line1 = new Line(10, 40, 40);
+    line1.draw();
+    ball = new Ball(40,75,new Vectoc(-1,2))
+    ball.draw()
+    list.push(line1)
+    list.push(ball)
+}
+
+
+function reDraw(e) {
+    Clear()
+    if (e.code == 'KeyW') {
+        if (list[0].y > 0) {
+            list[0].moveLine(list[0].x, list[0].y - 1)
+        }
+    }
+    else if (e.code == 'KeyS') {
+        if (list[0].y + list[0].lenght < 150) {
+            list[0].moveLine(list[0].x, list[0].y + 1);
+        }
+    }
+    Draw(list)
 }
